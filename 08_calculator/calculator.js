e912d7752db333dbd5c56b20eea4923c8af701b8
WSL-Veeraja <veeraja.v@gmail.com>
2024-10-13 14:51:02 +0530

    Finished JS08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let s = 0;
+	for (i = 0; i < arr.length; i++){
+    s += arr[i];
+  }
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  prod = 1;
+  arr.forEach(element => {
+    prod *= element;
+  });
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let fact = 1;
+  for (i = 2; i <= n; i++) fact *= i;
+  return fact;
 };
 
 // Do not edit below this line
