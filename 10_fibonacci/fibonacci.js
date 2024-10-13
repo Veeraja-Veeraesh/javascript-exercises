e712bfb3e74b03a93efd0ad41fd4ad8112c4b8db
WSL-Veeraja <veeraja.v@gmail.com>
2024-10-13 15:11:26 +0530

    Finished JS10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let a = 1;
+    let b = 1;
+    let s = 0; 
+    if (n < 0) return "OOPS"
+    if (n == 0) return 0;
+    if (n < 3) return a; 
+    for (i = 3; i <= n; i++){
+        s = a + b;
+        a = b;
+        b = s;
+    }
+    return s;
 };
 
 // Do not edit below this line
