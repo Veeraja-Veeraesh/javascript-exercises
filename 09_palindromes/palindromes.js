91d10665b838697c130293d6343ce56c65e9d85d
WSL-Veeraja <veeraja.v@gmail.com>
2024-10-13 15:06:31 +0530

    FInished JS09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let s2 = "";
+    s = s.toLowerCase();
+    for (const letter of s) {
+        if (letter >= 'a' && letter <= 'z' || letter >= '0' && letter <= '9') {
+            s2 += letter;
+        }
+    }
+    const reversedS2 = s2.split("").reverse().join("");
+    console.log(reversedS2);
+    return s2 === reversedS2;
 };
 
+console.log(palindromes("Animal loots foliated detail of stool lamina."));
 // Do not edit below this line
 module.exports = palindromes;
